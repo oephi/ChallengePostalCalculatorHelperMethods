0b32a75efaef0e0e264cf912f4d89ce922a0492c
oephi <r.sickle@protonmail.com>
2017-05-17 17:56:17 +1000

    Postal Calculator for Diploma Assignment
    Postal Calculator Using Methods
    
    Business Rule 1: Accept width, height and optionally the length of a
    parcel.
    Accept the shipping method - Ground, Air, Next Day.
    
    Business Rule 2: Once you have the minimum amount of information you
    need,
    produce a result on screen.  The result will be the volume of the
    package
    (width * height and optionally * length) multiplied by the "multiplier"
    for
    each shipping method.
    
    Ground: .15 multiplier
    Air: .25 multiplier
    Next Day: .45
    
    Non-Functional Requirement 1: You must name the project
    ChallengePostalCalculatorHelperMethods.
    
    Non-Functional Requirement 2: You must use methods.  Each method must do
    one thing
    and do it well.

---
+++ Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            Postal Calculator<br />
+            <br />
+            Width:&nbsp;
+            <asp:TextBox ID="widthTextBox" runat="server" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Height:&nbsp;
+            <asp:TextBox ID="heightTextBox" runat="server" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Length:&nbsp;
+            <asp:TextBox ID="lengthTextBox" runat="server" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundButton" runat="server" AutoPostBack="True" GroupName="deilveryButtons" OnCheckedChanged="groundButton_CheckedChange" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="airButton" runat="server" AutoPostBack="True" GroupName="deilveryButtons" OnCheckedChanged="airButton_CheckedChanged" Text="Air" />
+            <br />
+            <asp:RadioButton ID="nextDayButton" runat="server" AutoPostBack="True" GroupName="deilveryButtons" OnCheckedChanged="nextDayButton_CheckedChanged" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {   
+        //  Declare universal variables
+        int widthHeightLengthValue = 0;
+        double result = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                      
+            
+        }
+
+               
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+            calculateValues();
+        }
+
+        
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+           
+            calculateValues();
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+            calculateValues();
+        }
+
+        protected void groundButton_CheckedChange(object sender, EventArgs e)
+        {
+            
+            calculateValues();
+        }
+
+        protected void airButton_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            calculateValues();
+        }
+
+        protected void nextDayButton_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            calculateValues();
+        }
+
+
+
+        //  Method to calculate all input values
+        private void calculateValues()
+        {
+            // Declare variables for width, height and length values
+            int widthValue;
+            int heightValue;
+            int lengthValue;
+
+            //  Call hasValue method which checks if textboxes are empty
+            // and if buttons have been selected
+            hasValue();
+
+            //  Checks if textboxes value is an int, if false, returns nothing.
+            //  If true, passes value into respective variables
+            if (int.TryParse(widthTextBox.Text, out widthValue) 
+                && int.TryParse(heightTextBox.Text, out heightValue)
+                && int.TryParse(lengthTextBox.Text, out lengthValue))
+            {
+                widthHeightLengthValue = widthValue * heightValue* lengthValue;
+            } 
+            else if (int.TryParse(widthTextBox.Text, out widthValue)
+                && int.TryParse(heightTextBox.Text, out heightValue))
+            {
+                widthHeightLengthValue = widthValue * heightValue; 
+            }
+            else
+            {
+                return;
+            }
+
+            //  Call buttonIsChecked, checks which buttons have been selected
+            buttonIsChecked();
+
+            //  Returns result of calculateValue()
+            resultLabel.Text = result.ToString();
+        }
+
+
+
+        //  Method to check if any buttons are checked and if
+        //  textboxes have values
+        private void hasValue()
+        {
+            if (!groundButton.Checked
+                && !airButton.Checked
+                && !nextDayButton.Checked)
+                return;
+
+            if (widthTextBox.Text.Trim().Length == 0
+                || heightTextBox.Text.Trim().Length == 0)
+                return;
+            
+           
+        }
+    
+
+
+        //  Method to check WHICH buttons have been selected
+        private void buttonIsChecked()
+        {
+            if (groundButton.Checked) result = widthHeightLengthValue * .15;
+            else if (airButton.Checked) result = widthHeightLengthValue * .25;
+            else result = widthHeightLengthValue * .45;
+        }
+    }
+
+}
